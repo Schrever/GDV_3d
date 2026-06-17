53b21c4a2ec5449182c35228c7647ef1a62c201e
Schrever <jbb090410@gmail.com>
2026-06-17 12:02:57 +0200

    d
    dwa

--- Assets/Scripts/InputPlayer.cs
+++ Assets/Scripts/InputPlayer.cs
@@ -5,11 +5,17 @@ public class InputPlayer : MonoBehaviour
 {
     [SerializeField] private InputActionAsset IAA;
     [SerializeField] private string Mapname = "Player1";
+    [SerializeField] private float jumpForce = 5f;
+
     private InputActionMap IAM;
     private InputAction Jump;
     private InputAction Sprint;
     private InputAction Move;
-    private Rigidbody rigidbody;
+
+    private Rigidbody rb;
+    private Animator animator;
+
+    private bool isGrounded = true;
 
     void Awake()
     {
@@ -17,39 +23,39 @@ public class InputPlayer : MonoBehaviour
         Jump = IAM.FindAction("Jump");
         Sprint = IAM.FindAction("Sprint");
         Move = IAM.FindAction("Move");
-
-
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Start()
     {
-        rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Jump.WasPressedThisFrame())
-        {
-            //
-        }
-        else if (Jump.IsPressed())
-        {
-            rigidbody.AddForce(Vector3.up * 10);
-        }
-        else if (Jump.WasReleasedThisFrame())
-        {
-            //
-        }
-
-        if (Sprint.IsPressed())
-        {
-            //
-        }
-
         Vector2 moveinput = Move.ReadValue<Vector2>();
 
+        if (Jump.WasPressedThisFrame() && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+            animator.SetTrigger("Jump");
+        }
+
         transform.Translate(transform.forward * moveinput.y * 5f * Time.deltaTime, Space.World);
-        transform.Rotate(Vector3.up * moveinput.x * 100f * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.up * moveinput.x * 250f * Time.deltaTime, Space.World);
+
+        float speed = (moveinput.magnitude > 0) ? (Sprint.IsPressed() ? 2f : 1f) : 0f;
+
+        animator.SetFloat("Speed", speed);
+        animator.SetBool("Grounded", isGrounded);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 }
